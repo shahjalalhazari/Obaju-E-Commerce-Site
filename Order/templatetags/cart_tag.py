7c934838f175cf1a78ea34da782b103cd6607093
Shahjalal Hazari <shajalalhazari1@gmail.com>
2021-02-20 15:41:43 +0600

    Success page added.

--- Order/templatetags/cart_tag.py
+++ Order/templatetags/cart_tag.py
@@ -10,12 +10,3 @@ def item_total(user):
         return order[0].orderitems.count()
     else:
         return 0
-    
-    
-@register.filter
-def total_amount(user):
-    order = Order.objects.filter(user=user, ordered=False)[0]
-    sub_total = order.get_total_amount()
-    delivery_cost = order.delivery_method.cost
-    total_amount = float(sub_total) + float(delivery_cost)
-    return total_amount
